3182fa77f6bd4b8a880fadde44927af262a8529f
James Messinger <james.messinger@gmail.com>
2020-04-10 08:50:06 -0500

    Added IDs to all the apps

--- test/fixtures/integration-apps/carrier-typescript-app/src/carrier-typescript-app.ts
+++ test/fixtures/integration-apps/carrier-typescript-app/src/carrier-typescript-app.ts
@@ -6,6 +6,7 @@ import { ShippingProviderConfig } from "@shipengine/ipaas";
 export const config: ShippingProviderConfig = {
 
   type: "shipping_provider",
+  id: "d54ea601-9374-425c-8732-1f9eae4eae18",
   name: "My Carrier",
   description: "My Carrier description goes here",
   websiteURL: "https://www.my-carrier.com",
