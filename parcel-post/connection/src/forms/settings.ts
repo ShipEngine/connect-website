c1cd147d4f0bd7fc483fd1abc2f799f4091271de
James Messinger <james.messinger@gmail.com>
2020-05-09 10:53:16 -0500

    (WIP) Started implementing the Parcel Post connection app

---
+++ parcel-post/connection/src/forms/settings.ts
@@ -0,0 +1,66 @@
+"use strict";
+
+module.exports = {
+  dataSchema: {
+    title: "Parcel Post Settings",
+    description: "Update your Parcel Post account settings",
+    type: "object",
+    required: [
+      "default_currency",
+      "deliver_duties_paid"
+    ],
+    properties: {
+      default_currency: {
+        title: "Default currency",
+        type: "string",
+        enum: [
+          "USD",
+          "CAD",
+          "AUD",
+          "NZD",
+          "GBP",
+          "EUR"
+        ]
+      },
+      deliver_duties_paid: {
+        title: "Include duties & taxes in label cost",
+        type: "boolean"
+      }
+    }
+  },
+  uiSchema: {
+    default_currency: {
+      "ui:autofocus": true
+    }
+  },
+  localization: {
+    es: {
+      dataSchema: {
+        title: "Configuraciones de Parcel Post",
+        description: "Actualice la configuración de su cuenta de Parcel Post",
+        properties: {
+          default_currency: {
+            title: "Moneda predeterminada",
+          },
+          deliver_duties_paid: {
+            title: "Incluya aranceles e impuestos en el costo de la etiqueta"
+          }
+        }
+      }
+    },
+    fr: {
+      dataSchema: {
+        title: "Paramètres Parcel Post",
+        description: "Mettez à jour les paramètres de votre compte Parcel Post",
+        properties: {
+          default_currency: {
+            title: "Devise par Défaut"
+          },
+          deliver_duties_paid: {
+            title: "Inclure les droits et taxes dans le coût de l'étiquette"
+          }
+        }
+      }
+    }
+  }
+};
---
+++ parcel-post/connection/src/index.ts
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  id: "29291ae9-99f6-4a74-baa1-84058c3e28d2",
+  name: "Parcel Post",
+  description: "Parcel Post is a ground freight trucking company serving Central and North America.",
+  websiteURL: "https://parcel-post.net",
+  logo: "../logo.svg",
+
+  connectionForm: "forms/connect.ts",
+  settingsForm: "forms/settings.ts",
+
+  connect: "connect.ts",
+
+  localization: {
+    es: {
+      description: "Parcel Post es una compañía de transporte terrestre de carga que sirve a Centro y Norte América.",
+      websiteURL: "https://parcel-post.ca",
+    },
+    fr: {
+      description: "Parcel Post est une entreprise de camionnage de fret terrestre desservant l'Amérique centrale et l'Amérique du Nord.",
+      websiteURL: "https://parcel-post.com.mx",
+    },
+  },
+};
