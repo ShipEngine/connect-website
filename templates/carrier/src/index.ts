40070aa0f43f227349a9fbaa55090619676e91a8
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-09 09:45:26 -0500

    W.I.P.

--- templates/carrier/src/index.ts
+++ templates/carrier/src/index.ts
@@ -1,8 +1,8 @@
-import { CarrierApp } from "@shipengine/integration-platform-sdk";
+import { CarrierPOJO } from "@shipengine/integration-platform-sdk";
 import { createLabel } from "./methods/create-label";
 import { getRates } from "./methods/get-rates";
 
-export const App: CarrierApp = {
+export const carrier: CarrierPOJO = {
   id: "<%- _uuidv4 %>",
   name: "<%- pjson.name %>",
   description: "<%- pjson.description %>",
