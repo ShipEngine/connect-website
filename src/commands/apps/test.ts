9f2092661492c53cf61fd828037c7cb30e41c7dc
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-09 08:32:28 -0500

    W.I.P.

--- src/commands/apps/test.ts
+++ src/commands/apps/test.ts
@@ -1,4 +1,6 @@
 import BaseCommand from "../../base-command";
+import loader from "@shipengine/integration-platform-loader";
+// import path from "path";
 // import { flags } from "@oclif/command";
 
 export default class Test extends BaseCommand {
@@ -7,6 +9,8 @@ export default class Test extends BaseCommand {
   static examples = ["$ shipengine apps:test"];
 
   async run() {
-    this.log("testint 1, 2, 3");
+    const pathToApp = `${process.cwd()}`;
+    let app = await loader.loadApp(pathToApp);
+    this.log(`Successfully loaded ${app.name} v${app.version}`);
   }
 }
