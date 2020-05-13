1c96b40df01093b18d92eea41582eef2628bac86
Pierce Harmon <pierceh.dev@gmail.com>
2020-05-13 15:54:46 -0500

    add additional methods

---
+++ templates/carrier/methods/cancel-shipments.ts
@@ -0,0 +1,21 @@
+import {
+  ShipmentCancellation,
+  ShipmentCancellationConfirmationPOJO,
+  Transaction,
+} from "@shipengine/integration-platform-sdk";
+import { Session } from "./session";
+
+/**
+ * Cancels one or more shipments that were previously created. Depending on the carrier,
+ * this may include voiding labels, refunding charges, and/or removing the shipment from the day's manifest.
+ */
+export default async function cancelShipments(
+  transaction: Transaction<Session>,
+  shipments: ShipmentCancellation[],
+): Promise<ShipmentCancellationConfirmationPOJO[]> {
+  throw new Error("NotImplementedError");
+  // STEP 1: Validation
+  // STEP 2: Create the data that the carrier's API expects
+  // STEP 3: Call the carrier's API
+  // STEP 4: Create the output data that ShipEngine expects
+}
---
+++ templates/carrier/methods/track-shipment.ts
@@ -0,0 +1,20 @@
+import {
+  TrackingCriteria,
+  TrackingInfoPOJO,
+  Transaction,
+} from "@shipengine/integration-platform-sdk";
+import { Session } from "./session";
+
+/**
+ * Returns tracking information for a shipment
+ */
+export default async function trackShipment(
+  transaction: Transaction<Session>,
+  shipment: TrackingCriteria,
+): Promise<TrackingInfoPOJO> {
+  throw new Error("NotImplementedError");
+  // STEP 1: Validation
+  // STEP 2: Create the data that the carrier's API expects
+  // STEP 3: Call the carrier's API
+  // STEP 4: Create the output data that ShipEngine expects
+}
