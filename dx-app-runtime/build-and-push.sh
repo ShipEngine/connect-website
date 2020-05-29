64dd8e42490ce68fef37d04df06153ea4903e7fe
Josh <49000234+jeversmann-shipstation@users.noreply.github.com>
2020-05-29 14:11:19 -0500

    Fix&Use ./build-and-push.sh (#621)

--- dx-app-runtime/build-and-push.sh
+++ dx-app-runtime/build-and-push.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
-# Don't print our ECR password
-set -e
-$(aws ecr get-login --no-include-email)
-
 set -ex
+AWS_REGION=${AWS_REGION:-"us-east-1"}
 AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-"813448775391"}
 IMAGE_TAG=${IMAGE_TAG:-"shipping-dx-base"}
 IMAGE_REPO_NAME=${IMAGE_REPO_NAME:-"ipaas-dip-functions"}
 NEW_VERSION=$(npm version patch)
 
+aws ecr get-login-password \
+    --region $AWS_REGION \
+| docker login \
+    --username AWS \
+    --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
 docker build . \
-    -t $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-$NEW_VERSION \
-    -t $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-$NEW_VERSION
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-latest
+    -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-$NEW_VERSION \
+    -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-latest
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-$NEW_VERSION
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG-latest
