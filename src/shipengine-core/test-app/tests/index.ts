27d38a2e207311ee4cc3d6dcf7d20341fc0e9033
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-06-08 13:45:41 -0500

    check in for review

--- src/shipengine-core/test-app/tests/index.ts
+++ src/shipengine-core/test-app/tests/index.ts
@@ -9,4 +9,4 @@ export * from "./rate-shipment-test-suite";
 export * from "./schedule-pickup-test-suite";
 // export * from "./shipment-cancelled-test-suite";
 // export * from "./shipment-created-test-suite";
-// export * from "./track-shipment-test-suite";
+export * from "./track-shipment-test-suite";
--- src/shipengine-core/test-app/tests/track-shipment-test-suite.ts
+++ src/shipengine-core/test-app/tests/track-shipment-test-suite.ts
@@ -1,34 +1,47 @@
-// import { Suite, TestProp, expect } from "../tiny-test";
-// import { CarrierApp } from "@shipengine/integration-platform-sdk";
+import { Suite, TestProp, expect } from "../tiny-test";
+import { CarrierApp, TrackingCriteriaPOJO, TransactionPOJO } from "@shipengine/integration-platform-sdk";
 
-// type TrackShipmentProps = [];
+type TrackShipmentProps = [TransactionPOJO, TrackingCriteriaPOJO];
 
-// export class TrackShipmentTestSuite extends Suite {
-//   title = "trackShipment";
+export class TrackShipmentTestSuite extends Suite {
+  title = "trackShipment";
 
-//   tests() {
-//     const carrierApp = this.app as CarrierApp;
+  tests() {
+    const carrierApp = this.app as CarrierApp;
 
-//     return this.testProps().map((testProp) => {
-//       return this.test(testProp.title, async function () {
-//         let result, errorResult;
-//         try {
-//           carrierApp.trackShipment &&
-//             (result = await carrierApp.trackShipment(...testProp.props));
-//         } catch (error) {
-//           errorResult = error;
-//         } finally {
-//           expect(errorResult).to.be.undefined;
-//           expect(result).to.be.ok;
-//         }
-//       });
-//     });
-//   }
+    return this.testProps().map((testProp) => {
+      return this.test(testProp.title, async function () {
+        let result, errorResult;
+        try {
+          carrierApp.trackShipment &&
+            (result = await carrierApp.trackShipment(...testProp.props));
+        } catch (error) {
+          errorResult = error;
+        } finally {
+          expect(errorResult).to.be.undefined;
+          expect(result).to.be.ok;
+        }
+      });
+    });
+  }
 
-//   private testProps(): TestProp<TrackShipmentProps>[] {
-//     const carrierApp = this.app as CarrierApp;
-//     let props: TestProp<TrackShipmentProps>[] = [];
+  private testProps(): TestProp<TrackShipmentProps>[] {
+    // const carrierApp = this.app as CarrierApp;
+    let props: TestProp<TrackShipmentProps>[] = [];
 
-//     return props;
-//   }
-// }
+    const trackingPOJO: TrackingCriteriaPOJO = {
+      trackingNumber: "MJAYMC0WNI0WOFQXODOWMTOXNS41NJZA"
+    }
+
+    let title = "tracking a shipment";
+
+    title =  ` with ${trackingPOJO.trackingNumber}`;
+
+    props.push({
+      title,
+      props: [this.transaction, trackingPOJO]
+    })
+    
+    return props;
+  }
+}
