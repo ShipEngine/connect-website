7aeb3adc6bbdfd54022bc41c55db379a86efd071
Pierce Harmon <pierceh.dev@gmail.com>
2020-06-08 14:16:47 -0500

    W.I.P.

---
+++ cargo-inc/shipengine.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  connectionFormDataProps: {
+    account_id: "123",
+    account_email: "test@test.com",
+    account_password: "testing123",
+    agree_to_eula: true,
+  },
+  negateTests: [],
+};
