5aa5d5b770481c7b200d3df982e9cc2a3d2afd8a
bill-robbins-ss <bill.robbins@shipstation.com>
2020-06-10 12:59:08 -0500

    emit package version on /diagnostics/version (#647)
    * emit package version on /diagnostics/version
    
    * use npm env vars for pkg version
    
    * add env to version too

--- dx-app-runtime/src/routes/diagnostics/index.ts
+++ dx-app-runtime/src/routes/diagnostics/index.ts
@@ -3,9 +3,13 @@ import {IRouter, Router} from "express";
 const router: IRouter = Router();
 
 router.get('/version', (req, res) => {
+
   res.send({
     git_branch: process.env.GIT_BRANCH || 'Branch N/A',
-    git_sha: process.env.GIT_SHA || 'SHA N/A'
+    git_sha: process.env.GIT_SHA || 'SHA N/A',
+    package_version: process.env.npm_package_version,
+    env: process.env.NODE_ENV
+
   });
 });
 
