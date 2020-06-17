8ceac76cbc2389342f2b710c165a5752de65f515
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-06-17 10:46:32 -0500

    add shipment created test suite

--- src/core/test-app/tests/shipment-created-test-suite.ts
+++ src/core/test-app/tests/shipment-created-test-suite.ts
@@ -1,34 +1,97 @@
-// import { Suite, TestProp, expect } from "../tiny-test";
-// import { OrderApp } from "@shipengine/integration-platform-sdk";
+import { Suite, TestProp, expect } from "../tiny-test";
+import { OrderApp, TransactionPOJO, SalesOrderShipmentPOJO, SalesOrderPackagePOJO, SalesOrderPackageItemPOJO, QuantityUnit } from "@shipengine/integration-platform-sdk";
+import { buildAddressWithContactInfo } from '../factories/address';
 
-// type ShipmentCreatedProps = [];
+type ShipmentCreatedProps = [TransactionPOJO, SalesOrderShipmentPOJO];
 
-// export class ShipmentCreatedTestSuite extends Suite {
-//   title = "shipmentCreated";
+export class ShipmentCreatedTestSuite extends Suite {
+  title = "shipmentCreated";
 
-//   tests() {
-//     const orderApp = this.app as OrderApp;
+  tests() {
+    const orderApp = this.app as OrderApp;
 
-//     return this.testProps().map((testProp) => {
-//       return this.test(testProp.title, async function () {
-//         let result, errorResult;
-//         try {
-//           orderApp.shipmentCreated &&
-//             (result = await orderApp.shipmentCreated(...testProp.props));
-//         } catch (error) {
-//           errorResult = error;
-//         } finally {
-//           expect(errorResult).to.be.undefined;
-//           expect(result).to.be.ok;
-//         }
-//       });
-//     });
-//   }
+    return this.testProps().map((testProp) => {
+      return this.test(testProp.title, async function () {
+        let errorResult;
+        try {
+          orderApp.shipmentCreated &&
+            (await orderApp.shipmentCreated(...testProp.props));
+        } catch (error) {
+          errorResult = error;
+        } finally {
+          expect(errorResult).to.be.undefined;
+        }
+      });
+    });
+  }
 
-//   private testProps(): TestProp<ShipmentCreatedProps>[] {
-//     const orderApp = this.app as OrderApp;
-//     let props: TestProp<ShipmentCreatedProps>[] = [];
+  private testProps(): TestProp<ShipmentCreatedProps>[] {
+    let props: TestProp<ShipmentCreatedProps>[] = [];
 
-//     return props;
-//   }
-// }
+    if (this.staticConfig.methods && this.staticConfig.methods.shipmentCreated) {
+      for (let prop of this.staticConfig.methods.shipmentCreated) {
+        const title = composeTitle(prop);
+        props.push({
+          title,
+          props: [this.transaction, prop]
+        });
+      }
+    }
+
+    createStaticTests(props, this.transaction);
+
+    return props;
+  }
+}
+
+function composeTitle(prop: SalesOrderShipmentPOJO): string {
+  let title = "Shipment Created:";
+
+  if(prop.trackingNumber) {
+    title += ` with tracking number: ${prop.trackingNumber}`;
+  }
+
+  if(prop.shipFrom) {
+    title += ` shipping from: ${prop.shipFrom.country}`;
+  }
+
+  if (prop.shipTo) {
+    title += ` shipping to: ${prop.shipTo.country}`;
+  }
+
+  return title;
+}
+
+function createStaticTests(props: TestProp<ShipmentCreatedProps>[], transaction: TransactionPOJO): void {
+  const packageItem: SalesOrderPackageItemPOJO = {
+    salesOrder: {
+      id: "123456"
+    },
+    salesOrderItem: {
+      id: "1234556",
+      sku: "blah blah"
+    },
+    quantity: {
+      value: 3,
+      unit: QuantityUnit.Each
+    }
+  }
+
+  const packagePOJO: SalesOrderPackagePOJO = {
+    contents: [packageItem]
+  }
+
+  let salesOrderShipment: SalesOrderShipmentPOJO = {
+    shipFrom: buildAddressWithContactInfo("US-from")!,
+    shipTo: buildAddressWithContactInfo("US-to")!,
+    shipDateTime: new Date(),
+    packages: [packagePOJO]
+  }
+
+  const title = `Shipment Created: handles an invalid sales order id`;
+
+  props.push({
+    title: title,
+    props: [transaction, salesOrderShipment]
+  });
+}
