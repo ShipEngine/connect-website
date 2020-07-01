82f5cc62c46e90e418bbb333a51d912299ed682f
rkrauskopf <randall.krauskopf@gmail.com>
2020-07-01 10:56:03 -0500

    fix linting issues

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -3,7 +3,7 @@ import updateNotifier, { Settings, Package } from "update-notifier";
 const pjson = require("../../package.json") as Package;
 
 // Setup a weekly notification to let a user known if a new version of the cli is available for install
-const hook: Hook<"init"> = async function (options) {
+const hook: Hook<"init"> = async () => {
   const settings: Settings = {
     pkg: pjson,
     updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
