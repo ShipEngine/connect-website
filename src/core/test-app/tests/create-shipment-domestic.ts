bc62b8c500734881d6dd794481925e0629ddbe40
Pierce Harmon <pierceh.dev@gmail.com>
2020-07-14 10:17:42 -0500

    W.I.P.

--- src/core/test-app/tests/create-shipment-domestic.ts
+++ src/core/test-app/tests/create-shipment-domestic.ts
@@ -1,96 +1,101 @@
-import {
-  CarrierApp,
-  NewShipmentPOJO,
-  NewPackagePOJO,
-  WeightUnit,
-  ServiceArea,
-} from "@shipengine/integration-platform-sdk";
-import Suite from "../runner/suite";
-import { buildAddressWithContactInfo } from "../factories/address";
-import { MethodArgs } from "../runner/method-args";
-import { CreateShipmentDomesticOptions } from "../runner/config";
+// import {
+//   CarrierApp,
+//   NewShipmentPOJO,
+//   NewPackagePOJO,
+//   WeightUnit,
+//   ServiceArea,
+// } from "@shipengine/integration-platform-sdk";
+// import Suite from "../runner/suite";
+// import { buildAddressWithContactInfo } from "../factories/address";
+// import { MethodArgs } from "../runner/method-args";
+// import { CreateShipmentDomesticOptions } from "../runner/config";
 
-interface TestArgs {
-  methodArgs: MethodArgs<NewShipmentPOJO>;
-  config: any;
-}
+// interface TestArgs {
+//   methodArgs: MethodArgs<NewShipmentPOJO>;
+//   config: any;
+// }
 
-export class CreateShipmentDomestic extends Suite {
-  title = "createShipment_domestic";
+// export class CreateShipmentDomestic extends Suite {
+//   title = "createShipment_domestic";
 
-  buildTestArg(config: CreateShipmentDomesticOptions): TestArgs {
-    const carrierApp = this.app as CarrierApp;
+//   buildTestArg(config: CreateShipmentDomesticOptions): TestArgs {
+//     const carrierApp = this.app as CarrierApp;
 
-    const deliveryService = carrierApp.deliveryServices.find(
-      (deliveryService) => deliveryService.serviceArea === ServiceArea.Domestic,
-    );
+//     // Check to see if the config supplies one
 
-    const weightValue = config.weight?.value ? config.weight.value : 50.0;
-    const weightUnit = WeightUnit.Pounds;
-    const shipDateTime = new Date();
-    const shipFrom = buildAddressWithContactInfo("US-from");
-    const shipTo = buildAddressWithContactInfo("US-to");
-    const labelFormat = deliveryService.labelFormats[0];
-    const labelSize = deliveryService.labelSizes[0];
+//     // Check ServiceArea
 
-    const packagePOJO: NewPackagePOJO = {
-      deliveryConfirmation: {
-        id: deliveryService.deliveryConfirmations[0].id,
-      },
-      packaging: {
-        id: deliveryService.packaging[0].id,
-      },
-      label: {
-        size: labelSize,
-        format: labelFormat,
-      },
-      weight: {
-        value: weightValue,
-        unit: weightUnit,
-      },
-    };
+//     // Pick first one
+//     const deliveryService = carrierApp.deliveryServices.find(
+//       (deliveryService) => deliveryService.serviceArea === ServiceArea.Domestic,
+//     );
 
-    let newShipmentPOJO: NewShipmentPOJO = {
-      deliveryService: {
-        id: deliveryService.id,
-      },
-      shipFrom: shipFrom!,
-      shipTo: shipTo!,
-      shipDateTime: shipDateTime,
-      packages: [packagePOJO],
-    };
+//     const weightValue = config.weight?.value ? config.weight.value : 50.0;
+//     const weightUnit = WeightUnit.Pounds;
+//     const shipDateTime = new Date();
+//     const shipFrom = buildAddressWithContactInfo("US-from");
+//     const shipTo = buildAddressWithContactInfo("US-to");
+//     const labelFormat = deliveryService.labelFormats[0];
+//     const labelSize = deliveryService.labelSizes[0];
 
-    return {
-      methodArgs: [this.transaction, newShipmentPOJO],
-      config: this.config,
-    };
-  }
+//     const packagePOJO: NewPackagePOJO = {
+//       deliveryConfirmation: {
+//         id: deliveryService.deliveryConfirmations[0].id,
+//       },
+//       packaging: {
+//         id: deliveryService.packaging[0].id,
+//       },
+//       label: {
+//         size: labelSize,
+//         format: labelFormat,
+//       },
+//       weight: {
+//         value: weightValue,
+//         unit: weightUnit,
+//       },
+//     };
 
-  buildTestArgs(): TestArgs[] {
-    if (Array.isArray(this.config)) {
-      return this.config.map((config: CreateShipmentDomesticOptions) => {
-        return this.buildTestArg(config);
-      });
-    } else {
-      const config = this.config as CreateShipmentDomesticOptions;
+//     let newShipmentPOJO: NewShipmentPOJO = {
+//       deliveryService: {
+//         id: deliveryService.id,
+//       },
+//       shipFrom: shipFrom!,
+//       shipTo: shipTo!,
+//       shipDateTime: shipDateTime,
+//       packages: [packagePOJO],
+//     };
 
-      return [];
-    }
-  }
+//     return {
+//       methodArgs: [this.transaction, newShipmentPOJO],
+//       config: this.config,
+//     };
+//   }
 
-  tests() {
-    return this.buildTestArgs().map((testArg) => {
-      return this.test(
-        "it creates a new domestic shipment",
-        testArg.methodArgs,
-        testArg.config,
-        async () => {
-          const carrierApp = this.app as CarrierApp;
+//   buildTestArgs(): TestArgs[] {
+//     if (Array.isArray(this.config)) {
+//       return this.config.map((config: CreateShipmentDomesticOptions) => {
+//         return this.buildTestArg(config);
+//       });
+//     } else {
+//       const config = this.config as CreateShipmentDomesticOptions;
 
-          carrierApp.createShipment &&
-            (await carrierApp.createShipment(...testArg.methodArgs));
-        },
-      );
-    });
-  }
-}
+//       return [];
+//     }
+//   }
+
+//   tests() {
+//     return this.buildTestArgs().map((testArg) => {
+//       return this.test(
+//         "it creates a new domestic shipment",
+//         testArg.methodArgs,
+//         testArg.config,
+//         async () => {
+//           const carrierApp = this.app as CarrierApp;
+
+//           carrierApp.createShipment &&
+//             (await carrierApp.createShipment(...testArg.methodArgs));
+//         },
+//       );
+//     });
+//   }
+// }
---
+++ src/core/test-app/tests/create-shipment-international.ts
@@ -0,0 +1,126 @@
+import {
+  CarrierApp,
+  NewShipmentPOJO,
+  NewPackagePOJO,
+  WeightUnit,
+  ServiceArea,
+} from "@shipengine/integration-platform-sdk";
+import Suite from "../runner/suite";
+import { buildAddressWithContactInfo } from "../factories/address";
+import { MethodArgs } from "../runner/method-args";
+import { CreateShipmentDomesticOptions } from "../runner/config";
+import { initializeTimeStamps, getTimeTitle } from "../../utils/time-stamps";
+
+interface TestArgs {
+  title: string;
+  methodArgs: MethodArgs<NewShipmentPOJO>;
+  config: any;
+}
+
+// This code is terse. Find context/help below.
+// https://stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b
+const _getKeyValue_ = (key: string) => (obj: Record<string, any>) => obj[key];
+
+export class CreateShipmentInternational extends Suite {
+  title = "createShipment_international";
+
+  buildTestArg(config: CreateShipmentDomesticOptions): TestArgs {
+    const carrierApp = this.app as CarrierApp;
+
+    let deliveryService;
+    // Check to see if the config supplies one
+
+    // Check ServiceArea
+
+    // Pick first one
+    deliveryService = carrierApp.deliveryServices.find(
+      (deliveryService) => deliveryService.serviceArea === ServiceArea.Domestic,
+    );
+
+    if (!deliveryService)
+      throw new Error(
+        "delivery serivce can not be undefined for createShipment_international",
+      );
+
+    const defaults = {
+      labelFormat: deliveryService.labelFormats[0],
+      labelSize: deliveryService.labelSizes[0],
+      shipDateTime: "tomorrow",
+      shipFrom: buildAddressWithContactInfo("US-from"),
+      shipTo: buildAddressWithContactInfo("US-to"),
+      weightUnit: WeightUnit.Pounds,
+      weightValue: 50.0,
+    };
+
+    const testParams = { ...defaults, ...this.config };
+
+    initializeTimeStamps("US");
+
+    const packagePOJO: NewPackagePOJO = {
+      deliveryConfirmation: {
+        id: deliveryService.deliveryConfirmations[0].id,
+      },
+      packaging: {
+        id: deliveryService.packaging[0].id,
+      },
+      label: {
+        size: testParams.labelSize,
+        format: testParams.labelFormat,
+      },
+      weight: {
+        value: testParams.weightValue,
+        unit: testParams.weightUnit,
+      },
+    };
+
+    let newShipmentPOJO: NewShipmentPOJO = {
+      deliveryService: {
+        id: deliveryService.id,
+      },
+      shipFrom: testParams.shipFrom!,
+      shipTo: testParams.shipTo!,
+      shipDateTime: testParams.shipDateTime,
+      packages: [packagePOJO],
+    };
+
+    return {
+      title: `it creates a new international shipment with ${Object.keys(
+        testParams,
+      )
+        .map(function (k: any) {
+          return `${k}: ${_getKeyValue_(k)(testParams)}`;
+        })
+        .join(", ")}`,
+      methodArgs: [this.transaction, newShipmentPOJO],
+      config: this.config,
+    };
+  }
+
+  buildTestArgs(): TestArgs[] {
+    if (Array.isArray(this.config)) {
+      return this.config.map((config: CreateShipmentDomesticOptions) => {
+        return this.buildTestArg(config);
+      });
+    } else {
+      const config = this.config as CreateShipmentDomesticOptions;
+
+      return [this.buildTestArg(config)];
+    }
+  }
+
+  tests() {
+    return this.buildTestArgs().map((testArg) => {
+      return this.test(
+        testArg.title,
+        testArg.methodArgs,
+        testArg.config,
+        async () => {
+          const carrierApp = this.app as CarrierApp;
+
+          carrierApp.createShipment &&
+            (await carrierApp.createShipment(...testArg.methodArgs));
+        },
+      );
+    });
+  }
+}
