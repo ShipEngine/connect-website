0e0bb6474c35e742da5d8cdeb05ef2931281957d
Justin <46581173+justin-shipstation@users.noreply.github.com>
2020-07-20 10:40:05 -0500

    Fix error mapping dx runtime (#735)
    * Adding a mapping for the modules throwing an error.
    
    This should now surface the error messages being returned from the modules.
    
    * Added Stack to error details

--- dx-app-runtime/src/service-handler/index.ts
+++ dx-app-runtime/src/service-handler/index.ts
@@ -8,6 +8,34 @@ const getShipstationHeaders = (headers: IncomingHttpHeaders): any => {
   };
 };
 
+const mapDxErrorToHttpStatusCode = (errorCode: string): number => {
+  switch(errorCode) {
+    case 'ERR_UNAUTHORIZED':
+      return 401;
+    case 'ERR_RATE_LIMIT':
+      return 429;
+    case 'ERR_EXTERNAL_SERVER_ERROR':
+      return 520;
+    default:
+      return 400;
+  }
+};
+
+const mapErrorToResponse = (response: Response, error: any) => {
+  const errorCode = error.code || 'ERR_INVALID';
+  const message = error.message || 'Module Error Occurred';
+  const mappedError = {
+    detailed_errors: [
+      {
+        standardized_error_code: "mapping_error",
+        message: message,
+        details: { stack: error.stack, ...error },
+      },
+    ],
+  };
+  response.status(mapDxErrorToHttpStatusCode(errorCode)).send(mappedError);
+}
+
 export default (implementation: any, request: Request, response: Response) => {
   if (typeof implementation !== "function") {
     response.status(404).send("Not Supported");
@@ -20,9 +48,5 @@ export default (implementation: any, request: Request, response: Response) => {
   const dxApp = request.app.locals.app;
   implementation(dxApp, body, relevantHeaders)
     .then((result: any) => response.send(result))
-    .catch((error: any) => {
-      const statusCode = error.statusCode || 500;
-      const responseBody = error.body || error;
-      response.status(statusCode).send(responseBody);
-    });
+    .catch((error: any) => mapErrorToResponse(response, error));
 };
