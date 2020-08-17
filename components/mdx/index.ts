0fefcb72191cb27b9bcf891b2f85188e1d35a685
James Messinger <james.messinger@gmail.com>
2020-08-17 17:40:03 -0500

    Moved MDX components to a separate file

---
+++ components/mdx/index.ts
@@ -0,0 +1,30 @@
+import a from "./a";
+import blockquote from "./blockquote";
+import code from "./code";
+import del from "./del";
+import em from "./em";
+import h1 from "./h1";
+import h2 from "./h2";
+import h3 from "./h3";
+import h4 from "./h4";
+import h5 from "./h5";
+import h6 from "./h6";
+import hr from "./hr";
+import img from "./img";
+import inlineCode from "./inline-code";
+import li from "./li";
+import ol from "./ol";
+import p from "./p";
+import pre from "./pre";
+import strong from "./strong";
+import table from "./table";
+import tbody from "./tbody";
+import td from "./td";
+import thead from "./thead";
+import tr from "./tr";
+import ul from "./ul";
+
+// Customize the components that are used to render MDX pages
+export default {
+  a, p, h1, h2, h3, h4, h5, h6, blockquote, ul, ol, li, table, thead, tbody, tr, td, pre, code, em, strong, del, inlineCode, hr, img
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,8 @@
 import { MDXProvider } from "@mdx-js/react";
 import { AppProps } from "next/app";
 import { BaseLayoutProps } from "../components/layouts/base";
-import a from "../components/mdx/a";
-import blockquote from "../components/mdx/blockquote";
-import code from "../components/mdx/code";
-import del from "../components/mdx/del";
-import em from "../components/mdx/em";
-import h1 from "../components/mdx/h1";
-import h2 from "../components/mdx/h2";
-import h3 from "../components/mdx/h3";
-import h4 from "../components/mdx/h4";
-import h5 from "../components/mdx/h5";
-import h6 from "../components/mdx/h6";
-import hr from "../components/mdx/hr";
-import img from "../components/mdx/img";
-import inlineCode from "../components/mdx/inline-code";
-import li from "../components/mdx/li";
-import ol from "../components/mdx/ol";
-import p from "../components/mdx/p";
-import pre from "../components/mdx/pre";
-import strong from "../components/mdx/strong";
-import table from "../components/mdx/table";
-import tbody from "../components/mdx/tbody";
-import td from "../components/mdx/td";
-import thead from "../components/mdx/thead";
-import tr from "../components/mdx/tr";
-import ul from "../components/mdx/ul";
+import mdxComponents from "../components/mdx";
 import "../styles/global.scss";
-
-// Customize the components that are used to render MDX pages
-const mdxComponents = {
-  a, p, h1, h2, h3, h4, h5, h6, blockquote, ul, ol, li, table, thead, tbody, tr, td, pre, code, em, strong, del, inlineCode, hr, img
-};
 
 /**
  * The wrapper for all pages. Provides a place for site-wide functionality, such as error handling,
