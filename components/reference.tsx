04099d587413c84c3c5f88274cd958367f1f1932
James Messinger <james.messinger@gmail.com>
2020-08-23 10:39:55 -0500

    Added logic to normalize props across all fields

--- components/reference.tsx
+++ components/reference.tsx
@@ -26,7 +26,7 @@ export function Reference({ nameHeading, children }: ReferenceProps) {
   // Determine which columns to show
   const firstField = fields[0] || {} as FieldProps;
   const hasName = Boolean(firstField.name);
-  const hasType = Boolean(firstField.type || firstField.type);
+  const hasType = Boolean(firstField.type);
   const hasDefault = Boolean(firstField.defaultValue);
   const hasRequired = Boolean(firstField.required !== undefined);
   const hasNullable = Boolean(firstField.nullable !== undefined);
@@ -85,12 +85,13 @@ export function Description({ children }: { children: Children }) {
 
 
 /**
- * The `type`, `default`, and `description` can be specified as string props
+ * The `type`, `defaultValue`, and `description` can be specified as string props
  * or as JSX child components. This function normalizes them as props.
  */
 function normalizeChildren(children: Children<FieldProps>): FieldProps[] {
   const fields: FieldProps[] = [];
 
+  // Move <Type>, <DefaultValue>, and <Description> from children to props
   for (const field of getElements(children)) {
     const props = { ...field.props };
     fields.push(props);
@@ -111,6 +112,24 @@ function normalizeChildren(children: Children<FieldProps>): FieldProps[] {
           break;
       }
     }
+  }
+
+  // Determine which props are set on ANY of the fields
+  const hasName = fields.some(field => field.name);
+  const hasType = fields.some(field => field.type);
+  const hasDefault = fields.some(field => field.defaultValue);
+  const hasRequired = fields.some(field => field.required !== undefined);
+  const hasNullable = fields.some(field => field.nullable !== undefined);
+  const hasDescription = fields.some(field => field.description);
+
+  // Ensure that every field has the same props
+  for (const field of fields) {
+    hasName && field.name === undefined && (field.name = "");
+    hasType && field.type === undefined && (field.type = "");
+    hasDefault && field.defaultValue === undefined && (field.defaultValue = "");
+    hasRequired && field.required === undefined && (field.required = false);
+    hasNullable && field.nullable === undefined && (field.nullable = false);
+    hasDescription && field.description === undefined && (field.description = "");
   }
 
   return fields;
