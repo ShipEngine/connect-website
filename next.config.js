b7b93d290bc51af3f9c322e1308fd261fc6e1a70
Pierce Harmon <pierceh.dev@gmail.com>
2020-08-25 11:35:08 -0500

    fixup next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /// <reference path="./next-env.d.ts" />
 "use strict";
 
-const withMDX = require("@shipengine/next-mdx")();
+const withMDX = require("@shipengine/next-mdx")({
+  layoutsDir: "./src/layouts",
+  pagesDir: "./src/pages",
+});
 
 // Enable MDX in Next.js
 module.exports = withMDX({
   pageExtensions: ["tsx", "mdx"],
-  layoutsDir: "./src/layouts",
-  pagesDir: "./src/pages",
 });
