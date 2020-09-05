5926d572d8cb7b6fdfb32f592a3ea0aaeb77b7d9
James Messinger <james.messinger@gmail.com>
2020-09-05 10:15:26 -0500

    Removed the CommonJS default export hack, since it's unnecessary in this case

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,3 @@ export * from "./load-app";
 
 // The default export is an "Connect Loader" object, which has a `loadApp()` method
 export default { loadApp };
-
-// CommonJS default export hack
-/* eslint-env commonjs */
-if (typeof module === "object" && typeof module.exports === "object") {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-  module.exports = Object.assign(module.exports.default, module.exports);
-}
