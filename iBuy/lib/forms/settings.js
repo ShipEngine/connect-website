ed2ab124bdedb853178ec92879c90de09ec3bb1d
Pierce Harmon <pierceh.dev@gmail.com>
2020-09-15 09:53:59 -0500

    W.I.P.

--- iBuy/lib/forms/settings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const settingsForm = {
-    dataSchema: {},
-    uiSchema: {},
-};
-exports.default = settingsForm;
-//# sourceMappingURL=settings.js.map
