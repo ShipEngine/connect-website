18024994e2ebcc9196e3d2ffe952047b025758f5
Justin <46581173+justin-shipstation@users.noreply.github.com>
2020-09-25 11:03:11 -0500

    Adding Basic Auth Object to Runtime (#870)
    This will allow us to pass back something meaningful in the session.

--- dx-app-runtime/src/mapping/functions/register-response.ts
+++ dx-app-runtime/src/mapping/functions/register-response.ts
@@ -1,14 +1,36 @@
 import { Transaction } from '@shipengine/connect-sdk';
 import { RegisterResponse } from '@ipaas/capi/responses';
 
-export const mapRegisterResponse = (transaction: Transaction): RegisterResponse => {
-  const response: RegisterResponse = {
-    credentials: {
+interface BasicAuth {
+  userId?: string;
+  password?: string;
+}
+
+const getCredentials = (auth?: BasicAuth) => {
+  if(!auth) {
+    return {
       username: 'N/A',
       password: 'N/A',
-    },
+    }
+  } else {
+    return {
+      username: auth.userId || 'N/A',
+      password: auth.password || 'N/A'
+    }
+  }
+}
+
+export const mapRegisterResponse = (transaction: Transaction): RegisterResponse => {
+  const session = transaction.session as any;
+  const { _basicAuth } = session;
+  const credentials = getCredentials(_basicAuth);
+  
+  delete session._basicAuth;
+
+  const response: RegisterResponse = {
+    credentials,
     metadata: {
-      ...transaction.session,
+      ...session,
     },
   };
   return response;
