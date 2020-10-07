95e1c81c276bc1b23eac377c7fcc33df35336086
MarshalJoe <joseph.charles.marshall@gmail.com>
2020-10-07 17:17:04 -0500

    Adds scaffold spec for connection form acceptance test

---
+++ test/specs/core/test-app/tests/connection-form.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+"use strict";
+
+const { expect } = require("chai");
+
+describe("The connection form test suite", () => {
+
+	it("should throw an error if the react json schema is missing or misconfigured", () => {
+
+	});
+
+	it("should successfully be passed to connect function", () => {
+
+	});
+
+});
