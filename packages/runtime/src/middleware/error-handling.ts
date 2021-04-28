5c45d4d4b0e4b3f391c51819dee45caf184cac08
Matt Burton <matt.burton@gmail.com>
2021-04-28 14:19:22 -0400

    Add message to error response (#109)

--- packages/runtime/src/middleware/error-handling.ts
+++ packages/runtime/src/middleware/error-handling.ts
@@ -8,37 +8,24 @@ export default (error: any, request: Request, response: Response, next: NextFunc
   const transactionId = getTransactionId();
   if (error.isIntentional) {
     const handledError = error as BaseError;
-    if (handledError.details) {
-      response.status(handledError.statusCode).json({
-        transaction_id: transactionId,
-        detailed_errors: handledError.details.map((detail) => {
-          return {
-            external_error_code: detail.externalErrorCode,
-            message: detail.message || error.message,
-            external_http_status_code: detail.externalHttpStatusCode,
-            raw_external_context: detail.externalContext
-              ? JSON.stringify(serializeError(detail.externalContext))
-              : undefined,
-            standardized_error_code: detail.errorCode,
-          };
-        }),
-        retry_after_seconds: handledError.retryAfterSeconds,
-        retry_after_time: handledError.retryAfterTime,
-        throttling_context: handledError.throttlingContext,
-      });
-    } else {
-      response.status(handledError.statusCode).json({
-        transaction_id: transactionId,
-        detailed_errors: [
-          {
-            message: error.message,
-          },
-        ],
-        retry_after_seconds: handledError.retryAfterSeconds,
-        retry_after_time: handledError.retryAfterTime,
-        throttling_context: handledError.throttlingContext,
-      });
-    }
+    response.status(handledError.statusCode).json({
+      transaction_id: transactionId,
+      message: handledError.message,
+      detailed_errors: handledError.details?.map((detail) => {
+        return {
+          external_error_code: detail.externalErrorCode,
+          message: detail.message || error.message,
+          external_http_status_code: detail.externalHttpStatusCode,
+          raw_external_context: detail.externalContext
+            ? JSON.stringify(serializeError(detail.externalContext))
+            : undefined,
+          standardized_error_code: detail.errorCode,
+        };
+      }),
+      retry_after_seconds: handledError.retryAfterSeconds,
+      retry_after_time: handledError.retryAfterTime,
+      throttling_context: handledError.throttlingContext,
+    });
   } else {
     logger.error(serializeError(error));
     response.status(HttpStatusCode.ServerError).json({
