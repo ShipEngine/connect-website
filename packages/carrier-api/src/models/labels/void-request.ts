6778c4d8d2f0b50704fe903763e6a187cf24565b
Doug Wile <wile.doug@gmail.com>
2021-07-20 15:45:14 -0500

    feat: update void request with changes from capi v1.14.5 (#250)
    * feat: update void request with changes from capi v1.14.5
    
    * yarn format

--- packages/carrier-api/src/models/labels/void-request.ts
+++ packages/carrier-api/src/models/labels/void-request.ts
@@ -1,6 +1,8 @@
+import { ShippedShipment } from '..';
+import { ShipFrom } from '../addresses';
+
 /** @description Basic structure for a request to void a label */
-export interface VoidRequest {
+export interface VoidRequest extends ShippedShipment {
   void_request_id: string;
-  tracking_number?: string;
-  carrier_transaction_id?: string;
+  ship_from: ShipFrom;
 }
