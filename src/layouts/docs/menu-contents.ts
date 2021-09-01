89ec5e9c3bcec93d13425f5aa9ae70bad72eb4e8
Lewis Zhang <lewiszhang@gmail.com>
2021-09-01 18:17:09 -0500

    fix: App Structure links (#13)

--- src/layouts/docs/menu-contents.ts
+++ src/layouts/docs/menu-contents.ts
@@ -9,7 +9,7 @@ export const menu: MenuContents = [
         menuItems: [
           { title: "Welcome", href: "/" },
           { title: "Getting Started", href: "/docs/v2/getting-started" },
-          { title: "App Structure", href: "/docs//v2/structure" },
+          { title: "App Structure", href: "/docs/v2/structure" },
           { title: "Recommended Tools", href: "/docs/v2/tools" },
         ]
       },
